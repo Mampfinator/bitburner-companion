9e1d1d4c36e020a3f78e1ffe7af86bf1589a231c
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2024-08-05 18:37:13 +0200

    implement (WIP) function RAM usage display

--- src/ram-display/index.ts
+++ src/ram-display/index.ts
@@ -146,6 +146,14 @@ function* iterTokens(semanticTokens: vscode.SemanticTokens): Iterable<Tuple<numb
     }
 }
 
+function* skip<T>(iterable: Iterable<T>, n: number): Iterable<T> {
+    let i = 0;
+    for (const item of iterable) {
+        if (i++ >= n) {
+            yield item;
+        }
+    }
+}
 
 class RamDisplayCodeLensProvider implements vscode.CodeLensProvider, vscode.Disposable {
     public readonly logger: IChildLogger;
@@ -167,7 +175,7 @@ class RamDisplayCodeLensProvider implements vscode.CodeLensProvider, vscode.Disp
     }
 
     async provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.CodeLens[]> {
-        const tokens = await this.getRamUsingTokens(document);
+        const tokens = await this.getRamHintLocations(document);
         if (!tokens) {
             return [];
         }
@@ -181,60 +189,255 @@ class RamDisplayCodeLensProvider implements vscode.CodeLensProvider, vscode.Disp
         });
     }
 
-
-    public async getRamUsingTokens(document: vscode.TextDocument) {
+    public async getRamHintLocations(document: vscode.TextDocument): Promise<{line: number, char: number, length: number, cost: number}[]> {
+        // If we don't have TypeScript, we can't access hover/JSDoc information to parse the cost from.
         if (!(await vscode.languages.getLanguages()).includes("typescript")) {
-            return;
+            return [];
         }
+
+        const logger = this.logger;
 
         const tokens: { line: number, char: number, length: number, cost: number }[] = [];
 
         const legend = await vscode.commands.executeCommand<vscode.SemanticTokensLegend>("vscode.provideDocumentSemanticTokensLegend", document.uri);
+
+        const M_DECLARATION = legend.tokenModifiers.indexOf("declaration")!;
+
+        function isDeclaration(token: Tuple<number, 5> | number): boolean;
+        function isDeclaration(modifier: number): boolean;
+        function isDeclaration(tokenOrModifier: Tuple<number, 5> | number): boolean {
+            const modifierBits = typeof tokenOrModifier === "number" ? tokenOrModifier : tokenOrModifier[4];
+            return (modifierBits & (1 << M_DECLARATION)) !== 0;
+        }
+
+        const M_DEFAULT_LIBRARY = legend.tokenModifiers.indexOf("defaultLibrary")!;
+        function isDefaultLibrary(token: Tuple<number, 5> | number): boolean {
+            const modifierBits = typeof token === "number" ? token : token[4];
+            return (modifierBits & (1 << M_DEFAULT_LIBRARY)) !== 0;
+        }
+
+        const T_VARIABLE = legend.tokenTypes.indexOf("variable")!;
+        const T_PROPERTY = legend.tokenTypes.indexOf("property")!;
+        const T_FUNCTION = legend.tokenTypes.indexOf("function")!;
+        const T_METHOD = legend.tokenTypes.indexOf("method")!;
+
+
         const semanticTokens = await vscode.commands.executeCommand<vscode.SemanticTokens>("vscode.provideDocumentSemanticTokens", document.uri);
 
-        function getTokenName(line: number, char: number, length: number) {
+        async function getTokenName([line, char, length]: Tuple<number, 3>, uri: vscode.Uri): Promise<string> {
+            const document = await vscode.workspace.openTextDocument(uri);
             const token = document.lineAt(line).text.substring(char, char + length);
             return token;
         }
 
-        function getTokenModifiers(modifierBits: number): string[] {
-            const modifiers: string[] = [];
-            for (let i = 0; i < legend.tokenModifiers.length; i++) { 
-                if (modifierBits & (1 << i)) {
-                    modifiers.push(legend.tokenModifiers[i]);
+        async function getDefinitionTokens([line, char, length, type]: Tuple<number, 4>): Promise<{ token: Tuple<number, 5>, uri: vscode.Uri}[]> {
+            const locations: vscode.Location[] = await vscode.commands.executeCommand<(vscode.Location | vscode.LocationLink)[]>(
+                "vscode.executeDefinitionProvider", 
+                document.uri, 
+                new vscode.Position(line, char),
+            ).then(locations => locations.map(location => {
+                return location instanceof vscode.Location ? location : new vscode.Location(location.targetUri, location.targetRange);
+            }));
+
+            return Promise.all(
+                locations.map(async location => {
+                    return {
+                        token: [
+                            location.range.start.line,
+                            location.range.start.character,
+                            length,
+                            // type should be the same as where we came frome
+                            type,
+                            // modifier bits beyond `M_DECLARATION` don't matter, and since this location is definitely a declaration,
+                            // we don't need a semantic token lookup
+                            1 << M_DECLARATION,
+                        ], 
+                        uri: location.uri
+                    };
+                })
+            );
+        }
+
+        const costLookup = new Map<string, number | null>();
+
+        async function getHoverCost([line, char]: Tuple<number, 2>, uri: vscode.Uri): Promise<number | null> {
+            const hovers = await vscode.commands.executeCommand<vscode.Hover[]>("vscode.executeHoverProvider", uri, new vscode.Position(line, char));
+            
+            if (!hovers || hovers.length === 0) {
+                return null;
+            }
+
+            const hoverText = hovers.map(hover => hover.contents).flat().map(c => typeof c === "string" ? c : c.value).join("");
+
+            const costText = hoverText.match(/(?<=RAM cost\: *)\d+(\.\d+)?(?= *GB)/);
+            if (!costText || costText.length === 0) {
+                return null;
+            }
+
+            const cost = parseFloat(costText[0]);
+            if (isNaN(cost)) {
+                return null;
+            }
+
+            return cost;
+        }
+
+        async function getFunctionBody([line, char]: Tuple<number, 2>, uri: vscode.Uri): Promise<vscode.Range | null> {
+            logger.debug(`getFunctionBody started: ${line}, ${char}`);
+            
+            const document = await vscode.workspace.openTextDocument(uri);
+
+            let shouldMarkStart = false;
+            let start: vscode.Position | null = null;
+
+            let parenState = 0;
+
+            let lineText: string = document.lineAt(line).text;
+
+            while (lineText && (parenState !== 0 || !start)) {
+                if (lineText.length === 0) {
+                    try {
+                        line++;
+                        lineText = document.lineAt(line).text;
+                    } catch {
+                        // we've reached the end of the document
+                        return null;
+                    }
+                } else if (lineText[char] === "{") {
+                    parenState++;
+                    shouldMarkStart = true;
+                } else if (lineText[char] === "}") {
+                    parenState--;
+                } else {
+                    if (!start && shouldMarkStart) {
+                        start = new vscode.Position(line, char);
+                    }
+                }
+
+                char++;
+
+                if (char > lineText.length) {
+                    line++;
+                    try {
+                        lineText = document.lineAt(line).text;
+                    } catch {
+                        return null;
+                    }
+                    char = 0;
                 }
             }
 
-            return modifiers;
+            if (!start) {
+                console.error("Could not find start of function body");
+                return null;
+            }
+
+            return new vscode.Range(start, new vscode.Position(line, char));
+        }
+
+        async function getFunctionBodyCost([declarationLine, declarationChar]: Tuple<number, 2>, uri: vscode.Uri): Promise<number | null> {
+
+            const searchRange = await getFunctionBody([declarationLine, declarationChar], uri);
+            logger.debug(`getFunctionBodyCost: got search range ${JSON.stringify(searchRange)}`);
+            
+            if (!searchRange) {
+                return null;
+            }
+
+            const tokens = await vscode.commands.executeCommand<vscode.SemanticTokens>("vscode.provideDocumentRangeSemanticTokens", uri, searchRange);
+            if (!tokens) {
+                return null;
+            }
+            // prevent infinite loops in recursive functions.
+            // TODO: properly handle recursive functions; this is a hack as it prevents the cost from displayed above the recursive calls.
+            costLookup.set(`${uri.toString(true)}:${declarationLine}:${declarationChar}`, null);
+
+            let totalCost = 0;
+
+            // skip function declaration, since we'd end in an infinite loop otherwise
+            for (const token of iterTokens(tokens)) {
+                if (token[0] === declarationLine && token[1] === declarationChar) {
+                    continue;
+                }
+
+                const cost = await getTokenCost(token, uri);
+                if (cost !== null) {
+                    totalCost += cost;
+                }
+            }
+
+            return totalCost;
+        }
+
+        /**
+         * Recursively calculate the cost of a given token.
+         */
+        async function getTokenCost([line, char, length, type, modifier]: Tuple<number, 5>, uri = document.uri): Promise<number | null> {
+            if (isDefaultLibrary(modifier) && (type === T_VARIABLE || type === T_PROPERTY)) {
+                const name = await getTokenName([line, char, length], uri);
+                // window and document (document as property of window as well)
+                // are special cases that incur a 25 gig RAM cost
+                if (name === "window" || name === "document") {
+                    return 25;
+                }
+
+                return null;
+            }
+
+            if (type !== T_FUNCTION && type !== T_METHOD) {
+                return null;
+            }
+
+            if (!isDeclaration(modifier)) {
+                try {
+                    const declarations = await getDefinitionTokens([line, char, length, type]);
+
+                    for (const {token, uri} of declarations) {
+                        const cost = await getTokenCost(token, uri);
+                        if (cost !== null) {
+                            return cost;
+                        }
+                    }
+
+                    return null;
+                } catch (e) {
+                    console.error(e);
+                    return null;
+                }
+            }
+
+            const key = `${uri.toString(true)}:${line}:${char}`;
+
+            if (costLookup.has(key)) {
+                return costLookup.get(key)!;
+            }
+
+            // try getting cost from hover text first, as that's cheaper.
+            const hoverCost = await getHoverCost([line, char], uri);
+            if (hoverCost !== null) {
+                costLookup.set(key, hoverCost);
+                return hoverCost;
+            }
+
+            // find end of function body and calculate cost similarly
+            const bodyCost = await getFunctionBodyCost([line, char], uri);
+            if (bodyCost !== null) {
+                costLookup.set(key, bodyCost);
+                return bodyCost;
+            }
+
+            costLookup.set(key, null);
+            return null;
         }
 
         for (const [line, startChar, length, tokenType, modifiers] of iterTokens(semanticTokens)) {
-            console.log(getTokenName(line, startChar, length), legend.tokenTypes[tokenType], getTokenModifiers(modifiers));
-
-            if (legend.tokenTypes[tokenType] === "variable") {
-                const variableName = getTokenName(line, startChar, length);
-                if (variableName === "window" || variableName === "document") {
-                    tokens.push({ line: line, char: startChar, length, cost: 25 });
+            try {
+                const cost = await getTokenCost([line, startChar, length, tokenType, modifiers]);
+                if (cost !== null && cost > 0) {
+                    tokens.push({line, char: startChar, length, cost});
                 }
-            }
-
-            else if (legend.tokenTypes[tokenType] === "method" || (legend.tokenTypes[tokenType] === "property" && modifiers === 0)) {
-                const hovers = await vscode.commands.executeCommand<vscode.Hover[]>("vscode.executeHoverProvider", document.uri, new vscode.Position(line, startChar), undefined);
-                if (hovers && hovers.length > 0) {
-                    const hoverText = hovers.map(hover => hover.contents).flat().map(c => typeof c === "string" ? c : c.value).join("");
-
-                    const costText = hoverText.match(/(?<=RAM cost\: *)\d+(\.\d+)?(?= *GB)/);
-                    if (!costText) {
-                        continue;
-                    }
-
-                    const cost = parseFloat(costText[0]);
-                    if (isNaN(cost) || cost === 0) {
-                        continue;
-                    }
-
-                    tokens.push({ line: line, char: startChar, length, cost });
-                }
+            } catch (e) {
+                console.error(e);
             }
         }
 
